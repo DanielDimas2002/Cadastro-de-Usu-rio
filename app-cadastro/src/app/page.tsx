c6b8e78e04f5bb4541c02c0cfdbfc10e780c46ee
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2025-04-16 08:18:56 -0300

    Criando formu

--- app-cadastro/src/app/page.tsx
+++ app-cadastro/src/app/page.tsx
@@ -1,15 +1,20 @@
+'use client'
+
 // Importando o componente Pagina que você criou para envolver conteúdo
 import { IconUser } from "@tabler/icons-react";
 import Pagina from "./components/template/Pagina";
 import Titulo from "./components/template/Titulo";
 import ListaUsuario from "./components/usuario/ListaUsuario";
+import FormularioUsuario from "./components/usuario/FomularioUsuario";
+import usuarios from "./data/hooks/constants/usuarios";
 
 // Componente Home principal
 export default function Home() {
   return (
-    <Pagina className="flex flex-col gap-10">  {/* Passando "Opa" como children para o componente Pagina */}
+    <Pagina className="flex flex-col gap-10">  
       <Titulo icone={IconUser} princial="Usuários" secundario="Cadastro de Usuários"/>
-      <ListaUsuario />
+      {/*<ListaUsuario />*/}
+      <FormularioUsuario usuario={usuarios[0]}/>
     </Pagina>
   );
 }
